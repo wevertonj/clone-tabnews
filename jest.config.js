29082b4b79454af94a053c59249ae3bb2cf29e84
Weverton J. da Silva <19512397+wevertonj@users.noreply.github.com>
2024-12-11 17:16:20 -0300

    Change tests to test folder

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+/** @type {import('jest').Config} */
+const config = {
+    setupFilesAfterEnv: ["reflect-metadata"],
+    collectCoverageFrom: [
+      '<rootDir>/src/modules/**/*.ts',
+      '!<rootDir>/src/modules/**/presenter/**',
+      '!<rootDir>/src/**/index.ts',
+      '!<rootDir>/src/**/external/**',
+      '!<rootDir>/src/**/enums/**',
+    ],
+    coverageDirectory: "coverage",
+    coverageProvider: "babel",
+    moduleNameMapper: {
+      '@/test/(.+)': '<rootDir>/test/$1',
+      '@/(.+)': '<rootDir>/src/$1'
+    },
+    testMatch: ['**/*.spec.ts'],
+    roots: [
+      '<rootDir>/src',
+      '<rootDir>/test'
+    ],
+    transform: {
+      '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    clearMocks: true,
+  };
+  
+  module.exports = config;
