a19bfcdc8c9ef06804c6235a567e682c4f670445
Weverton J. da Silva <19512397+wevertonj@users.noreply.github.com>
2024-12-30 02:53:20 -0300

    Another fix for problems with de new ESLint config format...

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,12 +4,29 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import stylisticTs from '@stylistic/eslint-plugin-ts';
 import parserTs from '@typescript-eslint/parser';
+import {
+  dirname
+} from 'path';
+import {
+  fileURLToPath
+} from 'url';
+
+const __filename = fileURLToPath(
+  import.meta.url);
+const __dirname = dirname(__filename);
 
 export default tseslint.config({
     ignores: ['node_modules', '**/node_modules/**', '**/*.js', '**/*.d.ts'],
   }, {
     plugins: {
       '@stylistic/eslint-plugin-ts': stylisticTs
+    },
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
     },
   },
   eslint.configs.recommended,
@@ -18,7 +35,7 @@ export default tseslint.config({
       '@stylistic/eslint-plugin-ts/semi': ['error', 'always'],
       '@stylistic/eslint-plugin-ts/consistent-type-definitions': 'off',
       '@stylistic/eslint-plugin-ts/no-extraneous-class': 'off',
-      '@stylistic/eslint-plugin-ts/quotes': ['error', 'single']
+      '@stylistic/eslint-plugin-ts/quotes': ['error', 'single'],
     },
   },
 );
