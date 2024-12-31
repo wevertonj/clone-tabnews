a5469c146bc140ca9fb615619d0be37bd2b60c69
Weverton J. da Silva <19512397+wevertonj@users.noreply.github.com>
2024-12-31 18:37:15 -0300

    Add V2 test

--- src/modules/status/status.service.ts
+++ src/modules/status/status.service.ts
@@ -5,4 +5,8 @@ export class StatusService {
   getStatus() {
     return { message: 'É nois que voa bruxão!' };
   }
+
+  getStatusV2() {
+    return { message: 'É nois que voa bruxão! V2' };
+  }
 }
