6f91071ff597527b2e10da75e187f2255916e64f
Weverton J. da Silva <19512397+wevertonj@users.noreply.github.com>
2024-12-31 18:49:49 -0300

    Use FastifyAdapter in NestFactory.create and bind to all network interfaces

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,19 @@ addAlias('@', 'dist');
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { extractor } from '@/shared/utils';
 
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
   app.setGlobalPrefix('api');
   app.enableVersioning({
     type: VersioningType.CUSTOM,
     extractor,
     defaultVersion: process.env.DEFAULT_VERSION ?? '1',
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 void bootstrap();
