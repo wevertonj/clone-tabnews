a253450cf7cd4a5081bef4919ee139a2ba1499d9
Weverton J. da Silva <19512397+wevertonj@users.noreply.github.com>
2025-01-10 11:02:53 -0300

    handles database SSL connection

--- src/shared/infra/database/database.ts
+++ src/shared/infra/database/database.ts
@@ -30,6 +30,7 @@ export class Database {
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       port: Number(process.env.POSTGRES_PORT),
+      ssl: process.env.NODE_ENV === 'production' ? true : false,
     });
   }
 
