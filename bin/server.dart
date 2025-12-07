e8ccdc302b468cf532202c61c4ac1c1c1bd481be
Weverton J. da Silva <19512397+wevertonj@users.noreply.github.com>
2025-12-06 23:08:50 -0300

    Update server port configuration to use a fixed value in application.yaml and server.dart

--- bin/server.dart
+++ bin/server.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:clone_tabnews/vaden_application.dart';
 
 Future<void> main(List<String> args) async {
+  final port = Platform.environment['PORT'] ?? '8080';
+  
   final vaden = VadenApp();
   await vaden.setup();
-  final server = await vaden.run(args);
+  final server = await vaden.run([...args, '--port', port]);
   print('Server listening on port ${server.port}');
 }
 
